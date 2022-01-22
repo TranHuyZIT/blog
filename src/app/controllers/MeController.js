27b6cce17df24b4c2ae391b567cc6dea7ff065ce
TranHuy2k2 <giahuytran2002@gmail.com>
2022-01-22 15:07:21 +0700

    Updated check all

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -3,11 +3,26 @@ const Course = require('../models/Courses');
 class MeController{
     // GET /search
     storedCourses(req, res, next){
-        Course.find({}).lean()
-            .then(courses => res.render('me/stored-courses', {
-                courses: courses
-            }))
-            .catch(next);
+
+        Promise.all([Course.find({}).lean(), Course.countDocumentsDeleted().lean()])
+            .then(([courses, deletedCount]) => {
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: courses
+                })
+            })
+
+        // Course.countDocumentsDeleted().lean()
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(next);
+
+        // Course.find({}).lean()
+        //     .then(courses => res.render('me/stored-courses', {
+        //         courses: courses
+        //     }))
+        //     .catch(next);
         
     }
 
